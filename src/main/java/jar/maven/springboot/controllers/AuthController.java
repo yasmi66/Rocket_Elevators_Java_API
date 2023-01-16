ba613513c267960d086f50231c3ae7a377737121
yasmi66 <yasmina.suwed@outlook.com>
2023-01-16 00:21:06 -0500

    presque

--- src/main/java/jar/maven/springboot/controllers/AuthController.java
+++ src/main/java/jar/maven/springboot/controllers/AuthController.java
@@ -1,20 +1,22 @@
 package jar.maven.springboot.controllers;
 
-import io.jsonwebtoken.Jwts;
+
 import jakarta.validation.Valid;
 import jar.maven.springboot.auth.AuthRequest;
 import jar.maven.springboot.auth.AuthResponse;
 import jar.maven.springboot.auth.JwtUtil;
 import jar.maven.springboot.entities.User;
-import jar.maven.springboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
@@ -25,34 +27,23 @@ public class AuthController {
     AuthenticationManager authManager;
     @Autowired
     JwtUtil jwtUtil;
-    @Autowired
-    private UserRepository userRepository;
 
-    @RequestMapping(value = "/Authenticate", method = RequestMethod.POST)
+    @PostMapping("/auth")
     public ResponseEntity<?> authenticate(@RequestBody @Valid AuthRequest request) {
-//        try {
-//            Authentication authentication = authManager.authenticate(
-//                    new UsernamePasswordAuthenticationToken(
-//                            request.getEmail(), request.getPassword())
-//            );
-        User user = userRepository.findByEmail(request.getEmail()).orElse(null);
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found.");
-        }
-        // generate user token
-        String token = jwtUtil.generateAccessToken(user);
-        // return user token
-        return ResponseEntity.ok(new AuthResponse(Jwts.parser().setSigningKey(JwtUtil.secret).parseClaimsJws(token).getBody().getSubject(), token));
+        try {
+            Authentication authentication = authManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(), request.getPassword())
+            );
 
+            User user = (User) authentication.getPrincipal();
+            String accessToken = jwtUtil.generateAccessToken(user);
+            AuthResponse response = new AuthResponse(user.getEmail(), accessToken);
+            return ResponseEntity.ok().body(response);
+
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 }
-//            User user = (User) authentication.getPrincipal();
-//            String accessToken = jwtUtil.generateAccessToken(user);
-//            AuthResponse response = new AuthResponse(user.getEmail(), accessToken);
-//            return ResponseEntity.ok().body(response);
-//
-//        } catch (BadCredentialsException e) {
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//        }
-
 
